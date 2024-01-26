98d122733f59d4619ee4d81aab3979ace962889c
Daniel Bennett <danielpsalmbennett@yahoo.com>
2024-01-26 13:35:02 -0600

    added users.py view

--- PersonalPlannerAPI/apps.py
+++ PersonalPlannerAPI/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PersonalplannerapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'PersonalPLannerAPI'
+    name = 'PersonalPlannerAPI'
