1a28aee49c0f48c4a8018b054321267b219a04a4
Daniel Bennett <danielpsalmbennett@yahoo.com>
2024-01-26 11:04:56 -0600

    added dependencies

---
+++ PersonalPLannerAPI/models/ppuser.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class PPUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="pp_user")
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    zipcode = models.IntegerField()
